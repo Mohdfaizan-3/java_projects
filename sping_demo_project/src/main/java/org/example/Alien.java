8a402f8900773bd1e65e8d4844667dada38d4a13
Mohd Faizan <faizan.mohd09@gmail.com>
2024-06-14 11:10:05 +0530

    setter injection and ref attribute

--- sping_demo_project/src/main/java/org/example/Alien.java
+++ sping_demo_project/src/main/java/org/example/Alien.java
@@ -1,7 +1,26 @@
 package org.example;
 
 public class Alien {
-    int age = 1;
+    private int age;
+    private Laptop lap;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setLap(Laptop lap) {
+        this.lap = lap;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Laptop getLap() {
+        return lap;
+    }
+
+
     public void code () {
         System.out.println("coding");
         System.out.println(age);
--- sping_demo_project/src/main/java/org/example/App.java
+++ sping_demo_project/src/main/java/org/example/App.java
@@ -12,15 +12,22 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");//get mvn dependency "spring context"
-        Alien alien1 = (Alien)  context.getBean("alien");// create a spring.xml file in resource folder and add beam
-        alien1.code();
-        alien1.age = 2;
-        Alien alien2 = (Alien) context.getBean("alien");
-        alien2.code();
-        Alien alien3 = (Alien) context.getBean("alienNonMutable");
-        alien3.age = 3;
-        alien3.code();
-        Alien alien4 = (Alien) context.getBean("alienNonMutable");
-        alien4.code();
+        Alien alien1 = (Alien)  context.getBean("alienNonMutable");// create a spring.xml file in resource folder and add beam
+        System.out.println(alien1.getAge());//1 setter injection
+        alien1.getLap().working();//laptop// ref attribute
+
+
+
+        // singleton vs prototype
+//        Alien alien1 = (Alien)  context.getBean("alien");// create a spring.xml file in resource folder and add beam
+//        alien1.code();//1
+//        alien1.age = 2;
+//        Alien alien2 = (Alien) context.getBean("alien");
+//        alien2.code();//1
+//        Alien alien3 = (Alien) context.getBean("alienNonMutable");
+//        alien3.age = 3;
+//        alien3.code();//3
+//        Alien alien4 = (Alien) context.getBean("alienNonMutable");
+//        alien4.code();//1
     }
 }
