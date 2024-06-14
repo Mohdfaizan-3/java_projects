e84e96df2cb3abbb50108f7e0f36112995d308a2
Mohd Faizan <faizan.mohd09@gmail.com>
2024-06-14 17:12:09 +0530

    prototype

--- sping_demo_project/src/main/java/javaConfig/javaConfig.java
+++ sping_demo_project/src/main/java/javaConfig/javaConfig.java
@@ -3,11 +3,13 @@ package javaConfig;
 import org.example.Desktop;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class javaConfig {
 //    @Bean(name = {"beast", "desktop"}) here we can specfied bean name
     @Bean// default bean name destop()
+    @Scope("prototype")
     public Desktop desktop() {
         return new Desktop();
     }
