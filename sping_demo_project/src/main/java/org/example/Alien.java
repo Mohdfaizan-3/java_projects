cf7153c9a5e662d8fb8f709f33eb23d7d9d8cf9c
Mohd Faizan <faizan.mohd09@gmail.com>
2024-06-14 11:40:33 +0530

    constructuor injection

--- sping_demo_project/src/main/java/org/example/Alien.java
+++ sping_demo_project/src/main/java/org/example/Alien.java
@@ -3,9 +3,22 @@ package org.example;
 public class Alien {
     private int age;
     private Laptop lap;
+    int salary;
+
+    public Alien () {}
+
+    public Alien(int age, Laptop lap, int salary) {
+        this.age = age;
+        this.lap = lap;
+        this.salary = salary;
+    }
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public int getSalary() {
+        return salary;
     }
 
     public void setLap(Laptop lap) {
@@ -20,6 +33,9 @@ public class Alien {
         return lap;
     }
 
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 
     public void code () {
         System.out.println("coding");
