86720a7edf24129f6e4962b9c9460303c12f94c8
Mohd Faizan <faizan.mohd09@gmail.com>
2024-06-14 09:49:17 +0530

    spring xml config

--- sping_demo_project/src/main/java/org/example/App.java
+++ sping_demo_project/src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,8 @@ public class App
 {
     public static void main( String[] args )
     {
-      Alien alien1 = new Alien();
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");//get mvn dependency "spring context"
+        Alien alien1 = (Alien)  context.getBean("alien");// create a spring.xml file in resource folder and add beam
+        alien1.code();
     }
 }
