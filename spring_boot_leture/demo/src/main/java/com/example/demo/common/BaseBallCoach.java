9098b99e30c1a2bbd425d509b8d53c22933b871c
Mohd Faizan <faizan.mohd09@gmail.com>
2024-06-15 20:35:08 +0530

    life cycle

--- spring_boot_leture/demo/src/main/java/com/example/demo/common/BaseBallCoach.java
+++ spring_boot_leture/demo/src/main/java/com/example/demo/common/BaseBallCoach.java
@@ -1,5 +1,7 @@
 package com.example.demo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -9,5 +11,17 @@ import org.springframework.stereotype.Component;
 public class BaseBallCoach implements Coach{
     public String getDailyWorkOut() {
         return "30 min in baseball";
+    }
+
+    // define init method
+    @PostConstruct
+    public void doMyStartStuff() {
+        System.out.println("init");
+    }
+
+    // define init method
+    @PreDestroy
+    public void doCleanUpStuff() {
+        System.out.println("destroy");
     }
 }
