d699ae0675a04e2acf5f3fc81a6d2c1e9741b5dc
Mohd Faizan <faizan.mohd09@gmail.com>
2024-06-15 20:14:13 +0530

    Scope-prototype

--- spring_boot_leture/demo/src/main/java/com/example/demo/common/BaseBallCoach.java
+++ spring_boot_leture/demo/src/main/java/com/example/demo/common/BaseBallCoach.java
@@ -1,8 +1,11 @@
 package com.example.demo.common;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BaseBallCoach implements Coach{
     public String getDailyWorkOut() {
         return "30 min in baseball";
