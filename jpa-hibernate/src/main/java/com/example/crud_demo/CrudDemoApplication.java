0879c7c137b7b9fb2d41743762ce294ef93bf4a7
Mohd Faizan <faizan.mohd09@gmail.com>
2024-06-16 17:59:06 +0530

    setting and connected to mysql db

---
+++ jpa-hibernate/src/main/java/com/example/crud_demo/CrudDemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.crud_demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CrudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {//executed after the beans are loaded
+		return runner -> {
+			System.out.println("hello world");
+		};
+	}
+}
