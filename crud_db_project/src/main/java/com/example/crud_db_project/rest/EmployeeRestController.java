02532195843be9943dff86deb1add2a4eeed1e18
Mohd Faizan <faizan.mohd09@gmail.com>
2024-06-18 20:45:29 +0530

    all curd done using standard jpa

--- crud_db_project/src/main/java/com/example/crud_db_project/rest/EmployeeRestController.java
+++ crud_db_project/src/main/java/com/example/crud_db_project/rest/EmployeeRestController.java
@@ -3,9 +3,7 @@ package com.example.crud_db_project.rest;
 import com.example.crud_db_project.entity.Employee;
 import com.example.crud_db_project.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +20,37 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+        if (employee == null) {
+            throw new RuntimeException("employee id not found - " + employeeId);
+        }
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@PathVariable Employee employee) {
+        employee.setId(0);// in case client pass id to json
+        Employee employee1 = employeeService.save(employee);
+        return employee1;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        Employee employee1 = employeeService.save(employee);
+        return employee1;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int id) {
+        Employee employee = employeeService.findById(id);
+        if (employee == null) {
+            throw new RuntimeException("employee doesnt exit");
+        }
+        employeeService.deleteById(id);
+        return "deleted employee of id - " + id;
     }
 }
