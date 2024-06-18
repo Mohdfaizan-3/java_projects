67dc0a0cf5136cbf213e0f6aa0eeac9c8e2f52fa
Mohd Faizan <faizan.mohd09@gmail.com>
2024-06-18 17:26:14 +0530

    created REST controller to use DAO

---
+++ crud_db_project/src/main/java/com/example/crud_db_project/rest/EmployeeRestController.java
@@ -0,0 +1,24 @@
+package com.example.crud_db_project.rest;
+
+import com.example.crud_db_project.dao.EmployeeDAO;
+import com.example.crud_db_project.entity.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+    private EmployeeDAO employeeDAO;
+
+    public EmployeeRestController(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+}
