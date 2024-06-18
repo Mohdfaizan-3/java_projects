9255529b1d1e1001e8dc4257610990e19682b48f
Mohd Faizan <faizan.mohd09@gmail.com>
2024-06-18 13:47:18 +0530

    PathVariable and Error handling in REST

--- jpa-hibernate/src/main/java/com/example/crud_demo/rest/StudentRestController.java
+++ jpa-hibernate/src/main/java/com/example/crud_demo/rest/StudentRestController.java
@@ -1,8 +1,10 @@
 package com.example.crud_demo.rest;
 
 import com.example.crud_demo.rest.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -11,13 +13,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/students")
 public class StudentRestController {
+    ArrayList<Student> students;
 
-    @GetMapping("/student")
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
         students.add(new Student("ravi", 19212));
         students.add(new Student("ram", 19213));
         students.add(new Student("rai", 19214));
-        return students;
+    }
+
+    @GetMapping("/student/{studentId}")
+    public Student getStudents(@PathVariable int studentId) {
+        if (studentId >= students.size() || studentId < 0) {
+            throw new StudentNotFoundException("student not found - " + studentId);
+        }
+        return students.get(studentId);// studentId is index
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
