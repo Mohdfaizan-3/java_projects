736e865a1b535bdcb372567b27bfc10984eeb5e8
Mohd Faizan <faizan.mohd09@gmail.com>
2024-06-19 21:00:59 +0530

    crud security ristrict url

--- crud_db_project - spring-jpa -REST/src/main/java/com/example/crud_db_project/dao/EmployeeRepository.java
+++ crud_db_project - spring-jpa -REST/src/main/java/com/example/crud_db_project/dao/EmployeeRepository.java
@@ -2,9 +2,8 @@ package com.example.crud_db_project.dao;
 
 import com.example.crud_db_project.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "members")
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
--- crud_db_project - spring-jpa -REST/src/main/java/com/example/crud_db_project/security/DemoSecurityConfig.java
+++ crud_db_project - spring-jpa -REST/src/main/java/com/example/crud_db_project/security/DemoSecurityConfig.java
@@ -2,10 +2,18 @@ package com.example.crud_db_project.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
+/*
+@Bean is used on a method to indicate that it returns an object that
+should be registered as a bean in the Spring application context.
+*/
 @Configuration
 public class DemoSecurityConfig {
 
@@ -30,5 +38,21 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/magic-api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/magic-api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/magic-api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/magic-api/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/magic-api/employees/**").hasRole("ADMIN")
+        );
+        http.httpBasic(Customizer.withDefaults());
+        // disabling csrf. in general not require for stateless rest apis that uses POST, PUT, DELETE PATCH.
+        http.csrf(csrf -> csrf.disable());
+        return http.build();
     }
 }
