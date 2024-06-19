0fe3e35b263c21ee06aed15df6c05be2675f3f66
Mohd Faizan <faizan.mohd09@gmail.com>
2024-06-19 09:07:58 +0530

    spring data jpa done

---
+++ crud_db_project - spring-jpa/src/main/java/com/example/crud_db_project/dao/EmployeeRepository.java
@@ -0,0 +1,2 @@
+package com.example.crud_db_project.dao;public interface EmployeeRepository {
+}
--- crud_db_project/src/main/java/com/example/crud_db_project/dao/EmployeeDAO.java
+++ crud_db_project/src/main/java/com/example/crud_db_project/dao/EmployeeDAO.java
@@ -4,7 +4,8 @@ import com.example.crud_db_project.entity.Employee;
 
 import java.util.List;
 
-//DAO (Data Access Object): DAO is a design pattern that provides an abstract interface to some type of
+//DAO (Data Access Object): DAO is a design pattern that provides an abstract
+// interface to some type of
 // database or other persistence mechanism. It provides specific data operations
 // without exposing details of the database.
 public interface EmployeeDAO {
