d5b9d4239339e34884f419fb22b9e6dfad497bb3
Mohd Faizan <faizan.mohd09@gmail.com>
2024-06-19 10:19:32 +0530

    config and sorting spring data reset

--- crud_db_project - spring-jpa -REST/src/main/java/com/example/crud_db_project/dao/EmployeeRepository.java
+++ crud_db_project - spring-jpa -REST/src/main/java/com/example/crud_db_project/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.example.crud_db_project.dao;
 
 import com.example.crud_db_project.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    
+
 }
