9783f778d224b5b5adb7fdef0a23af4093d35bb5
Mohd Faizan <faizan.mohd09@gmail.com>
2024-06-19 09:43:41 +0530

    crud using spring data rest

---
+++ crud_db_project - spring-jpa -REST/src/test/java/com/example/crud_db_project/CrudDbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_db_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDbProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
