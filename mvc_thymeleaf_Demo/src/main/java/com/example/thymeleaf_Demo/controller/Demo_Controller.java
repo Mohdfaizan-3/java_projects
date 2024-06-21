b2eb77bb0319811609d860ce8d38089d88ef8ce7
Mohd Faizan <faizan.mohd09@gmail.com>
2024-06-21 12:36:22 +0530

    adding thymeleaf html page

---
+++ mvc_thymeleaf_Demo/src/main/java/com/example/thymeleaf_Demo/controller/Demo_Controller.java
@@ -0,0 +1,14 @@
+package com.example.thymeleaf_Demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Demo_Controller {
+    @GetMapping("/hello")
+    public String sayHello(Model model) {
+        model.addAttribute("theDate", java.time.LocalDateTime.now());
+        return "helloWorld";
+    }
+}
