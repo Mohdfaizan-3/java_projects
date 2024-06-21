814f09b6de2aa22d1e0b6566f2e16f989c040502
Mohd Faizan <faizan.mohd09@gmail.com>
2024-06-21 22:28:41 +0530

    added form validation

---
+++ Validation/src/main/java/com/example/Validation/Customer.java
@@ -0,0 +1,42 @@
+package com.example.Validation;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class Customer {
+    private String firstName;
+
+    @NotNull(message = "is Required")
+    @Size(min = 1, message = "is Required")
+    private String lastName = "";
+
+    @Min(value = 0, message = "must be greater equal than 0")
+    @Max(value = 10, message = "must be less than equal to 10")
+    private int freePasses;
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
