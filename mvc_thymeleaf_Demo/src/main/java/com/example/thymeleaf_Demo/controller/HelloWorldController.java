f8ef54f394ba5530cb513f46d6d896241811422b
Mohd Faizan <faizan.mohd09@gmail.com>
2024-06-21 16:45:57 +0530

    added form thymeleaf mvc

--- mvc_thymeleaf_Demo/src/main/java/com/example/thymeleaf_Demo/controller/HelloWorldController.java
+++ mvc_thymeleaf_Demo/src/main/java/com/example/thymeleaf_Demo/controller/HelloWorldController.java
@@ -4,6 +4,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -13,16 +15,10 @@ public class HelloWorldController {
         return "helloworld-form";
     }
 
-//    @GetMapping("/processForm")
-//    public String processForm() {
-//        return "helloWorld";
-//    }
 
-    @GetMapping("/processFormVersionTwo")
-    //read form data
-    public String readData(HttpServletRequest request, Model model) {
-        // read the req param from html form
-        String name = request.getParameter("studentName");
+    @PostMapping("/processFormVersionThree")
+//    //read form data
+    public String readData(@RequestParam("studentName") String name, Model model) {
         // convert the data to caps
         name = name.toUpperCase();
         // create the message
@@ -32,6 +28,25 @@ public class HelloWorldController {
         return "helloworld";
     }
 
+//
+//    @GetMapping("/processFormVersionTwo")
+//    //read form data
+//    public String readData(HttpServletRequest request, Model model) {
+//        // read the req param from html form
+//        String name = request.getParameter("studentName");
+//        // convert the data to caps
+//        name = name.toUpperCase();
+//        // create the message
+//        String result = "hi " + name;
+//        // add message to model
+//        model.addAttribute("message", result);
+//        return "helloworld";
+//    }
+
+//    @GetMapping("/processForm")
+//    public String processForm() {
+//        return "helloWorld";
+//    }
 
     // add data to model
 
