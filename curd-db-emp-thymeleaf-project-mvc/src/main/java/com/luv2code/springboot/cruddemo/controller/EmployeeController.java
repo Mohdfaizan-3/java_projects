6a0cab0b4a17302af7647f23cdc789df48e4bed5
Mohd Faizan <faizan.mohd09@gmail.com>
2024-06-22 15:24:06 +0530

    added employee

--- curd-db-emp-thymeleaf-project-mvc/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
+++ curd-db-emp-thymeleaf-project-mvc/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -29,5 +31,18 @@ public class EmployeeController {
         // add to spring model
         model.addAttribute("employees", employeeList);
         return "list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.save(employee);
+        return "redirect:/employees/list";// prevent duplicate saving
     }
 }
