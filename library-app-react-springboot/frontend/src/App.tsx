8884558dc2ca2fc1640238de87e345e8b5c39ac8
Mohd Faizan <faizan.mohd09@gmail.com>
2024-06-23 21:58:03 +0530

    start libray-project

---
+++ library-app-react-springboot/frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
+    <div className="container-fluid">
+     <span className='navbar-brand'></span>
+     <button className='navbar-toggle' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNavDropdown' aria-controls='navbarNavDropdown' aria-expanded='false' aria-label='Toggle Navigation'></button>
+    </div>
+    </nav>
+  );
+}
+
+export default App;
