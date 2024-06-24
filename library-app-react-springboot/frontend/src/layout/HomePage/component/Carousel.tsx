871ea22c867f25087fb2cd502abc8f035872ad59
Mohd Faizan <faizan.mohd09@gmail.com>
2024-06-24 17:16:57 +0530

    fetch carousel data from backend

--- library-app-react-springboot/frontend/src/layout/HomePage/component/Carousel.tsx
+++ library-app-react-springboot/frontend/src/layout/HomePage/component/Carousel.tsx
@@ -1,6 +1,45 @@
+import { useEffect, useState } from "react";
 import { ReturnBook } from "./ReturnBook";
+import BookModel from "../../../models/BookModel";
 
 export const Carousel = () => {
+
+  const [books, setBooks] = useState<BookModel[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  useEffect(()=>{
+    const fetchBooks = async () => {
+        const baseUrl: string = "http://localhost:8080/api/books";
+        const url : string = `${baseUrl}?page=0&size=9`;
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("something went wrong");
+        } 
+        const json = await response.json();
+        const data = await json._embedded.books;
+        const loadedBooks: BookModel[] = [];
+        for (const key in data) {
+            loadedBooks.push({
+              id : data[key].title,
+              title : data[key].title,
+              author : data[key].author,
+              description : data[key].description,
+              copies : data[key].copies,
+              copiesAvailable : data[key].copiesAvailable,
+              category : data[key].category,
+              img : data[key].img
+            })
+        }
+        setBooks(loadedBooks);
+        setLoading(false);
+
+    }
+    fetchBooks().catch((error: any)=>{
+      setLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
   return (
     <div className="container mt-5" style={{ height: 550 }}>
       <div className="homepage-carousel-title">
