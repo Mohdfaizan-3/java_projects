0126c6b6150dbd5835f9df7f35c6586fead7b9a8
Mohd Faizan <faizan.mohd09@gmail.com>
2024-06-25 20:50:38 +0530

    pagination react

--- library-app-react-springboot/frontend/src/layout/searchBookPage/SearchBookPage.tsx
+++ library-app-react-springboot/frontend/src/layout/searchBookPage/SearchBookPage.tsx
@@ -1,43 +1,57 @@
 import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
+import Pagination from "../utils/Pagination";
 import { SearchBook } from "./components/SearchBook";
 
 export const SearchBookPage = () => {
   const [books, setBooks] = useState<BookModel[]>([]);
   const [loading, setLoading] = useState(true);
-  const [httpError, setHttpError] = useState(null);
+  const [httpError, setHttpError] = useState<string | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [booksPerPage] = useState(2);
+  const [totalAmountOfBooks, setTotalAmountOfBooks] = useState(0);
+  const [totalPage, setTotalPage] = useState(0);
 
   useEffect(() => {
     const fetchBooks = async () => {
-      const baseUrl: string = "http://localhost:8080/api/books";
-      const url: string = `${baseUrl}?page=0&size=5`;
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error("something went wrong");
+      try {
+        const baseUrl: string = "http://localhost:8080/api/books";
+        const url: string = `${baseUrl}?page=${
+          currentPage - 1
+        }&size=${booksPerPage}`;
+        const response = await fetch(url);
+
+        if (!response.ok) {
+          throw new Error("Something went wrong!");
+        }
+
+        const responseJson = await response.json();
+        const data = responseJson._embedded.books;
+        setTotalAmountOfBooks(responseJson.page.totalElements);
+        setTotalPage(responseJson.page.totalPages);
+
+        const loadedBooks: BookModel[] = data.map((book: any) => ({
+          id: book.id,
+          title: book.title,
+          author: book.author,
+          description: book.description,
+          copies: book.copies,
+          copiesAvailable: book.copiesAvailable,
+          category: book.category,
+          img: book.img,
+        }));
+
+        setBooks(loadedBooks);
+        setLoading(false);
+      } catch (error: any) {
+        setLoading(false);
+        setHttpError(error.message);
       }
-      const responseJson = await response.json();
-      const data = await responseJson._embedded.books;
-      const loadedBooks: BookModel[] = [];
-      for (const key in data) {
-        loadedBooks.push({
-          id: data[key].id,
-          title: data[key].title,
-          author: data[key].author,
-          description: data[key].description,
-          copies: data[key].copies,
-          copiesAvailable: data[key].copiesAvailable,
-          category: data[key].category,
-          img: data[key].img,
-        });
-      }
-      setBooks(loadedBooks);
-      setLoading(false);
     };
-    fetchBooks().catch((error: any) => {
-      setLoading(false);
-      setHttpError(error.message);
-    });
-  }, []);
+
+    fetchBooks();
+    window.scrollTo(0, 0);
+  }, [currentPage, booksPerPage]);
 
   if (loading) {
     return <div className="container m-5">Loading....</div>;
@@ -47,56 +61,93 @@ export const SearchBookPage = () => {
     return <div className="container m-5">{httpError}</div>;
   }
 
-  return <div>
-    <div className="container">
+  const indexOfLastBook: number = currentPage * booksPerPage;
+  const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
+  const lastItem =
+    booksPerPage * currentPage <= totalAmountOfBooks
+      ? booksPerPage * currentPage
+      : totalAmountOfBooks;
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  return (
+    <div>
+      <div className="container">
         <div>
-            <div className="row mt-5 ">
-                <div className="col-6">
-                    <div className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="search" aria-labelledby="search" />
-                        <button className="btn btn-outline-success">Search</button>
-                    </div>
-                </div>
-                <div className="col-4">
-                    <div className="dropdown">
-                        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuBtn1" data-bs-toggle="dropdown" aria-expanded="false">Category</button>
-                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuBtn1">
-                            <li>
-                                <a className="dropdown-item" href="#">
-                                    ALl
-                                </a>
-                            </li>
-                            <li>
-                                <a className="dropdown-item" href="#">
-                                    Frontend
-                                </a>
-                            </li><li>
-                                <a className="dropdown-item" href="#">
-                                    Backend
-                                </a>
-                            </li><li>
-                                <a className="dropdown-item" href="#">
-                                    Data
-                                </a>
-                            </li><li>
-                                <a className="dropdown-item" href="#">
-                                    Devops
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+          <div className="row mt-5">
+            <div className="col-6">
+              <div className="d-flex">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="search"
+                  aria-labelledby="search"
+                />
+                <button className="btn btn-outline-success">Search</button>
+              </div>
             </div>
-            <div className="mt-3">
-                <h5>Number of results : (22)</h5>
+            <div className="col-4">
+              <div className="dropdown">
+                <button
+                  className="btn btn-secondary dropdown-toggle"
+                  type="button"
+                  id="dropdownMenuBtn1"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Category
+                </button>
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="dropdownMenuBtn1"
+                >
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      All
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Frontend
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Backend
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Data
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      DevOps
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <p>1 to 5 of 22 items</p>
-            {
-                books.map(book => (
-                    <SearchBook book={book} key={book.id}/>
-                )) 
-            }
+          </div>
+          <div className="mt-3">
+            <h5>Number of results : ({totalAmountOfBooks})</h5>
+          </div>
+          <p>
+            {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items
+          </p>
+          {books.map((book) => (
+            <SearchBook book={book} key={book.id} />
+          ))}
+          {totalPage > 1 && (
+            <Pagination
+              currentPage={currentPage}
+              totalPage={totalPage}
+              paginate={paginate}
+            />
+          )}
         </div>
+      </div>
     </div>
-  </div>;
+  );
 };
---
+++ library-app-react-springboot/frontend/src/layout/utils/Pagination.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+const Pagination: React.FC<{
+  currentPage: number;
+  totalPage: number;
+  paginate: (pageNumber: number) => void;
+}> = (props) => {
+  const pageNumber = [];
+
+  if (props.currentPage === 1) {
+    pageNumber.push(props.currentPage);
+    if (props.totalPage >= props.currentPage + 1) {
+      pageNumber.push(props.currentPage + 1);
+      if (props.totalPage >= props.currentPage + 2) {
+        pageNumber.push(props.currentPage + 2);
+      }
+    }
+  } else if (props.currentPage > 1) {
+    if (props.currentPage >= 3) {
+      pageNumber.push(props.currentPage - 2);
+      pageNumber.push(props.currentPage - 1);
+    } else {
+      pageNumber.push(props.currentPage - 1);
+    }
+    pageNumber.push(props.currentPage);
+
+    if (props.totalPage >= props.currentPage + 1) {
+      pageNumber.push(props.currentPage + 1);
+    }
+
+    if (props.totalPage >= props.currentPage + 2) {
+      pageNumber.push(props.currentPage + 2);
+    }
+  }
+
+  return (
+    <nav aria-label="Pagination">
+      <ul className="pagination">
+        <li className="page-item" onClick={() => props.paginate(1)}>
+          <button className="page-link">First Page</button>
+        </li>
+        {pageNumber.map((number) => (
+          <li
+            key={number}
+            onClick={() => props.paginate(number)}
+            className={
+              "page-item" + (props.currentPage === number ? " active" : "")
+            }
+          >
+            <button className="page-link">{number}</button>
+          </li>
+        ))}
+        <li
+          className="page-item"
+          onClick={() => props.paginate(props.totalPage)}
+        >
+          <button className="page-link">Last Page</button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
