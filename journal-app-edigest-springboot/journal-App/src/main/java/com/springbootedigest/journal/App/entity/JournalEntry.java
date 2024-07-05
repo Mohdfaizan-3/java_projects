520367f1edd19193ecc61ecad4aaf2a27a957f55
Mohd Faizan <faizan.mohd09@gmail.com>
2024-07-05 20:22:57 +0530

    Lombok

--- journal-app-edigest-springboot/journal-App/src/main/java/com/springbootedigest/journal/App/entity/JournalEntry.java
+++ journal-app-edigest-springboot/journal-App/src/main/java/com/springbootedigest/journal/App/entity/JournalEntry.java
@@ -1,5 +1,6 @@
 package com.springbootedigest.journal.App.entity;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,6 +10,7 @@ import java.util.Date;
 
 //@Document(collection = "journal_entries")
 @Document
+@Data
 public class JournalEntry {
 
     @Id
@@ -17,35 +19,4 @@ public class JournalEntry {
     private String content;
     private LocalDateTime date;
 
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId  id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
